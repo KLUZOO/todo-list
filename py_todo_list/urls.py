6d87fc7f9540df44a298c7591f5255f8d312c7da
Sasha <kluzodota@gmail.com>
2025-07-15 17:19:45 +0300

    task is done

--- py_todo_list/urls.py
+++ py_todo_list/urls.py
@@ -17,10 +17,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from tasks.views import TagsListView
+from tasks.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    ToggleCompleteTaskView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tags/', TagsListView.as_view(), name='index'),
-    path('tags/', TagsListView.as_view(), name='tags-list'),
+    path('', TaskListView.as_view(), name='index'),
+    path('task/create', TaskCreateView.as_view(), name='task-create'),
+    path('task/<int:pk>/update/', TaskUpdateView.as_view(), name='task-update'),
+    path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
+    path('task/<int:pk>/toggle/', ToggleCompleteTaskView.as_view(), name='task-toggle'),
+    path('tags/', TagListView.as_view(), name='tags-list'),
+    path('tags/create/', TagCreateView.as_view(), name='tag-create'),
+    path('tag/<int:pk>/update/', TagUpdateView.as_view(), name='tag-update'),
+    path('tag/<int:pk>/delete/', TagDeleteView.as_view(), name='tag-delete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,64 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 
 from tasks.models import Tag, Task
 
 
 class TaskListView(generic.ListView):
     model = Task
+    fields = "__all__"
+    template_name = "tasks/index.html"
 
 
-class TagsListView(generic.ListView):
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("index")
+
+
+class ToggleCompleteTaskView(generic.DetailView):
+    model = Task
+    template_name = "tasks/index.html"
+
+    def post(self, request, *args, **kwargs):
+        task = Task.objects.get(pk=self.kwargs["pk"])
+        task.is_done = not task.is_done
+        task.save()
+        return HttpResponseRedirect(reverse_lazy("index"))
+
+
+class TagListView(generic.ListView):
     model = Tag
     fields = "__all__"
     queryset = Tag.objects.prefetch_related('tasks').all()
+    template_name = "tasks/tag_list.html"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tags-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tags-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("tags-list")
